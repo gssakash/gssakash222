0fe7b30095db6acf1ac99fc7cb51dda750a75f82
Akash <gsaisriram.akash@gmail.com>
2020-04-12 07:44:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+
+
+/* ScrollReveal.JS Animations */
+
+
+
+window.sr = ScrollReveal();
+
+
+sr.reveal('.nav-container', {
+  duration:3500,
+  origin:'bottom'
+});
+
+sr.reveal('.social-icons', {
+    duration:3500,
+    origin:'bottom'
+  });
+
+sr.reveal('.home-left', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.home-image', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+});
+
+sr.reveal('.cover-image-2', {
+    duration:3000,
+    origin:'left',
+    distance:'300px'
+});
+
+sr.reveal('.about-left', {
+    duration:3000,
+    origin:'right',
+    distance:'300px'
+});
+
+sr.reveal('.container-contact', {
+    duration:3000,
+    origin:'bottom',
+    distance:'300px'
+});
+
